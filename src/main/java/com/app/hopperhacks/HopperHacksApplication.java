a30cd5a92e245f4f2b780743f7b4e3468237a215
손성민 <alexzzanga7407@gmail.com>
2025-02-21 22:13:02 -0500

    first commit

---
+++ src/main/java/com/app/hopperhacks/HopperHacksApplication.java
@@ -0,0 +1,13 @@
+package com.app.hopperhacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopperHacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopperHacksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/hopperhacks/HopperHacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hopperhacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopperHacksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
