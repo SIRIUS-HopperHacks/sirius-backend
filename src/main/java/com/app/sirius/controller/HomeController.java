0b23347198fd69f176b8e0049a914a8c3f32dcfc
손성민 <alexzzanga7407@gmail.com>
2025-02-22 23:21:00 -0500

    Update API specification

--- src/main/java/com/app/sirius/controller/HomeController.java
+++ src/main/java/com/app/sirius/controller/HomeController.java
@@ -1,16 +1,18 @@
 package com.app.sirius.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
+@Tag(name = "Home API", description = "Endpoints for the home page and general navigation")
 public class HomeController {
     public HomeController(){System.out.println("###LOG : "+getClass().getName() + "() 생성");}
 
     @RequestMapping("/home")
+    @Operation(summary = "Display Home Page", description = "Renders the home page for general navigation.")
     public void home(){}
-
 
 }
