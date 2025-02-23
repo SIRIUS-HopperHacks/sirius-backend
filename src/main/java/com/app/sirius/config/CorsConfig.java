0332de47a2aa79097ef9877e66b98e9ecc02fd65
손성민 <alexzzanga7407@gmail.com>
2025-02-23 07:58:00 -0500

    Update CORS Error

--- src/main/java/com/app/sirius/config/CorsConfig.java
+++ src/main/java/com/app/sirius/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // React 앱의 주소
+                        .allowedOrigins("http://localhost:5000", "http://localhost:8000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
